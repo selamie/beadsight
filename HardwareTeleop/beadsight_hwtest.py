33822681f4f17f1f8c3a9a39d3e688fa464bc8e6
Selam <selamg@andrew.cmu.edu>
2024-09-25 18:28:38 -0400

    robot desk changes, data collection tweaks

--- HardwareTeleop/beadsight_hwtest.py
+++ HardwareTeleop/beadsight_hwtest.py
@@ -1,4 +1,4 @@
-from beadsight_controller.beadsight_realtime import BeadSight
+from beadsight_realtime import BeadSight
 # from save_data import DataRecorder, cv2_dispay
 import cv2
 
@@ -7,16 +7,21 @@ if __name__ == '__main__':
 
     # test1()
 
-    beadcam = BeadSight(36)
-    while(True):
-        r, im = beadcam.get_frame()
-        if r:
-            # cv2.imshow('og',og)
-            cv2.imshow('unwarped',im)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-        else:
-            break
+    for i in range(36):
+        try:
+            beadcam = BeadSight(i)
+            while(True):
+                r, im = beadcam.get_frame()
+                if r:
+                    # cv2.imshow('og',og)
+                    cv2.imshow('unwarped',im)
+                    if cv2.waitKey(1) & 0xFF == ord('q'):
+                        print("CAM_NUM:",i)
+                        break
+                else:
+                    break
+            beadcam.cap.release()
+            cv2.destroyAllWindows()
+        except:
+            Exception("try again")
     
-    beadcam.cap.release()
-    cv2.destroyAllWindows()
