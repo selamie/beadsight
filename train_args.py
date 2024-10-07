a222870c52b786e1faae0b76497dab7782699be1
selamg <selamg@andrew.cmu.edu>
2024-10-07 14:19:32 -0400

    set up for training downstream task

--- train_args.py
+++ train_args.py
@@ -5,6 +5,7 @@ import os
 START_TIME = datetime.now()
 
 
+
 #NODE 3 clip:
 DATA_TYPE = "_stonehenge"
 CKPT_DIR = '/home/selam/beadsight_data/revision_checkpoints/'
@@ -14,7 +15,7 @@ IMAGE_WEIGHTS_PATH = '/home/selam/model_weights/epoch_1499_vision_encoder.pth'
 #not used for resnet
 DATA_DIR = "/home/selam/processed_data/"
 CODE_START_DIR = '/home/selam/beadsight' 
-ENC_TYPE = 'clip' 
+ENC_TYPE = 'resnet' 
 DEVICE_STR = 'cuda:0'
 PRED_HORIZON = 20
 ABLATE_BEAD = False
