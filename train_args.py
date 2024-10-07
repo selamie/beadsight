d7932d961b3f04ef50fd30c6a020cd1470e394f1
selamg <selamg@andrew.cmu.edu>
2024-10-07 14:21:16 -0400

    to cluster

--- train_args.py
+++ train_args.py
@@ -15,12 +15,13 @@ IMAGE_WEIGHTS_PATH = '/home/selam/model_weights/epoch_1499_vision_encoder.pth'
 #not used for resnet
 DATA_DIR = "/home/selam/processed_data/"
 CODE_START_DIR = '/home/selam/beadsight' 
-ENC_TYPE = 'resnet' 
+ENC_TYPE = 'clip' 
 DEVICE_STR = 'cuda:0'
 PRED_HORIZON = 20
-ABLATE_BEAD = False
-BEAD_ONLY = False
+ABLATE_BEAD = True
 FREEZE_BEAD = False
+
+BEAD_ONLY = False #not gonna mess with this
 
 # # local testing:
 # DATA_TYPE = "_augmentedFrozen"
