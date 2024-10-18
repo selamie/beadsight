5200e4f69f50489dfc51a86ffea40942dbd28bcf
selamg <selamg@andrew.cmu.edu>
2024-10-18 15:47:09 -0400

    to cluster

--- train_args.py
+++ train_args.py
@@ -10,8 +10,8 @@ START_TIME = datetime.now()
 DATA_TYPE = "ishape" # only put dataset type here, the rest is handled below
 CKPT_DIR = '/home/selam/beadsight_data/revision_checkpoints/'
 #for pretrained clip head
-BEADSIGHT_WEIGHTS_PATH = '/home/selam/model_weights/epoch_1499_beadsight_encoder.pth'
-IMAGE_WEIGHTS_PATH = '/home/selam/model_weights/epoch_1499_vision_encoder.pth'
+BEADSIGHT_WEIGHTS_PATH = '/home/selam/model_weights/stonehenge_epoch1499_beadsight_encoder.pth'
+IMAGE_WEIGHTS_PATH = '/home/selam/model_weights/stonehenge_epoch1499_vision_encoder.pth'
 #not used for resnet
 DATA_DIR = "/home/selam/processed_ishape_2/"
 CODE_START_DIR = '/home/selam/beadsight' 
