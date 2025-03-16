b78c76568fc9dce196f7a8f313a7228bb52e415c
Selam <selamg@andrew.cmu.edu>
2025-03-16 18:23:55 -0400

    fixed collected data

--- generate_norm_stats.py
+++ generate_norm_stats.py
@@ -4,7 +4,7 @@ import json
 
 # dataset_dir = '/home/selamg/beadsight/data/ssd/processed_ishape'
 dataset_dir = '/media/selamg/Crucial/selam/processed_flowarts'
-norm_stats = get_norm_stats(dataset_dir,num_episodes=43)
+norm_stats = get_norm_stats(dataset_dir,num_episodes=42)
 print(norm_stats)
 
 for i in norm_stats.keys():
